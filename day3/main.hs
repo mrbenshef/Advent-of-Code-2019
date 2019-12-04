058d4e96f4940d5fbde27a3c6126e7351e817baa
Ben Sheffield <bshefb@gmail.com>
2019-12-04 00:37:05 +0000

    day 3 part 2

--- day3/main.hs
+++ day3/main.hs
@@ -6,6 +6,8 @@ import           Control.Applicative
 
 import qualified Data.List                     as List
 import qualified Data.Set                      as Set
+import qualified Data.Map                      as Map
+import           Data.Map                       ( Map )
 
 data Direction = R Int | D Int | U Int | L Int deriving (Show)
 
@@ -13,6 +15,7 @@ main = do
   paths <- BS.lines <$> BS.readFile "input"
   let [path1, path2] = map (map (parseDir . BS.unpack) . BS.split ',') paths
   print $ minIntersection path1 path2
+  print $ minSteps path1 path2
 
 parseDir :: String -> Direction
 parseDir ('R' : r) = R (read r)
@@ -20,24 +23,31 @@ parseDir ('D' : d) = D (read d)
 parseDir ('U' : u) = U (read u)
 parseDir ('L' : l) = L (read l)
 
-allCords :: (Int, Int, Int) -> [Direction] -> [(Int, Int, Int)]
-allCords _ [] = []
+allCords :: (Int, Int, Int) -> [Direction] -> Map (Int, Int) Int
+allCords _ [] = Map.empty
 allCords (x, y, s) (R r : ds) =
-  [ (x + r', y, s + r') | r' <- [1 .. r] ] ++ allCords (x + r, y, s + r) ds
+  Map.fromList [ ((x + r', y), s + r') | r' <- [1 .. r] ]
+    `Map.union` allCords (x + r, y, s + r) ds
 allCords (x, y, s) (D d : ds) =
-  [ (x, y - d', s + d') | d' <- [1 .. d] ] ++ allCords (x, y - d, s + d) ds
+  Map.fromList [ ((x, y - d'), s + d') | d' <- [1 .. d] ]
+    `Map.union` allCords (x, y - d, s + d) ds
 allCords (x, y, s) (U u : ds) =
-  [ (x, y + u', s + u') | u' <- [1 .. u] ] ++ allCords (x, y + u, s + u) ds
+  Map.fromList [ ((x, y + u'), s + u') | u' <- [1 .. u] ]
+    `Map.union` allCords (x, y + u, s + u) ds
 allCords (x, y, s) (L l : ds) =
-  [ (x - l', y, s + l') | l' <- [1 .. l] ] ++ allCords (x - l, y, s + l) ds
+  Map.fromList [ ((x - l', y), s + l') | l' <- [1 .. l] ]
+    `Map.union` allCords (x - l, y, s + l) ds
 
 minIntersection :: [Direction] -> [Direction] -> Int
-minIntersection line1 line2 =
-  Set.findMin
-    $                  Set.map (\(x, y) -> abs x + abs y)
-    $                  dt1 ls1
-    `Set.intersection` dt1 ls2
- where
-  dt1 = Set.map (\(a, b, c) -> (a, b))
-  ls1 = Set.fromList (allCords (0, 0, 0) line1)
-  ls2 = Set.fromList (allCords (0, 0, 0) line2)
+minIntersection path1 path2 =
+  List.minimum
+    $                  map (\(x, y) -> abs x + abs y)
+    $                  Map.keys
+    $                  allCords (0, 0, 0) path1
+    `Map.intersection` allCords (0, 0, 0) path2
+
+minSteps :: [Direction] -> [Direction] -> Int
+minSteps path1 path2 = List.minimum $ Map.elems $ Map.intersectionWith
+  (+)
+  (allCords (0, 0, 0) path1)
+  (allCords (0, 0, 0) path2)
